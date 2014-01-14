f1cdc8aa961a163e0e5d9c0eb68b0062a607104d
Jia Xu <dolremi@gmail.com>
2014-01-14 10:59:04 -0800

    Little modification

--- Array_Strings/SetZero/main.cpp
+++ Array_Strings/SetZero/main.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 
-/* The problem is if an element in an M*N matrix is 0, set its entire row and column to zero
-   The initial attempt is to set all the rows and columns zero if one element found zero, then
-   all the matrix is zero now. Therefore we should at first record which element is zero in original 
-   matrix and which row or column it belongs, then set them zero altogether. 
-*/
-
+// The problem is if an element in an M*N matrix is 0, set its entire row and column to zero
+//   The initial attempt is to set all the rows and columns zero if one element found zero, then
+//   all the matrix is zero now. Therefore we should at first record which element is zero in original 
+//   matrix and which row or column it belongs, then set them zero altogether. 
 using namespace std;
 
 void setZero(int **input, int m, int n){
-
   // Two arrays to record the row and the column the zero element belongs to 
   int *rows = new int[m];
   int *cols = new int[n];
-
     
   for(int i = 0; i < m; i++){
     for (int j = 0; j < n; j++){
@@ -51,11 +47,9 @@ int main(int argc, char *argv[]){
   cin >> m >> n;
   
   input = new int*[m];
-  
   for(int i = 0; i < m; i++){
     input[i] = new int[n];
   }
-
 
   for(int i = 0; i < m; i++){
     for(int j = 0; j < n; j++){
@@ -64,34 +58,26 @@ int main(int argc, char *argv[]){
   }
 
   cout << "The original matrix is :" << endl;
-  
   for(int i = 0; i < m; i++){
     for(int j = 0; j < n; j++){
       cout << input[i][j] << " ";
     }
     cout << "\n";
   }
-
   cout << "\n";
 
   setZero(input, m, n);
-  
   for(int i = 0; i < m; i++){
     for(int j = 0; j < n; j++){
       cout << input[i][j] << " ";
     }
     cout << "\n";
   }
-
   cout << "\n";
 
-
   // Don't forget deallocate the memory
-
   for(int i = 0; i < m ;  i++)
     delete [] input[i];
-  
   delete [] input;
-
   return 0;
 }
